30e6d5a33c12db740d8fd567e114edb2b3d487e4
Shivam Ranjan <ranjanshivam9@gmail.com>
2021-02-05 16:55:54 +0530

    bmi cal completed

--- app.js
+++ app.js
@@ -16,6 +16,22 @@ app.post('/', function (req, res) {
 	res.send('Answer is ' + result);
 });
 
+// Request for the bmi calculator
+app.get('/bmi', (req, res) => {
+	res.sendFile(__dirname + '/bmi.html');
+});
+
+// response from the server to the calculator
+app.post('/bmi', (req, res) => {
+	var height = parseFloat(req.body.height);
+	var weight = parseFloat(req.body.weight);
+
+	var result = weight / (height * height);
+	// rounf of to 2 digits
+	result = result.toFixed(2);
+	res.send('BMI is ' + result);
+});
+
 app.listen(3000, function () {
-	console.log('server runing');
+	console.log('server running');
 });
